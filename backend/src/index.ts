3a784cbfd5a8f500639b98e4ecaf44c4ddb28001
Sherex <ingar@i-h.no>
2020-08-05 23:19:20 +0200

    Added addIp & deleteIp

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
 import { config } from './lib/config'
 import { log } from './lib/logger'
 import { checkPrerequisites } from './lib/check-prerequisites'
-import { createInterface, deleteInterface, getInterface } from './lib/iproute2'
-import { isInstalled, install, startServer, stopServer } from './lib/rqlite'
+import * as ip from './lib/iproute2'
+import * as rqlite from './lib/rqlite'
 
 if (config.interactive) {
   console.log('The --interactive switch is a planned feature! Exiting..')
@@ -14,23 +14,27 @@ if (config.interactive) {
   await checkPrerequisites()
 
   log('info', ['index', 'creating interface'])
-  await createInterface('wg0')
+  await ip.createInterface('wg0')
 
   log('debug', ['index', 'checking rqlite server'])
-  if (!await isInstalled()) await install(config.rqlite.version)
+  if (!await rqlite.isInstalled()) await rqlite.install(config.rqlite.version)
 
   log('debug', ['index', 'starting rqlite server'])
-  await startServer()
+  await rqlite.startServer()
 
-  console.log(await getInterface('wg0'))
+  await ip.addIp('wg0', '10.50.0.20/24')
+
+  console.log(await ip.getInterface('wg0'))
 
   await new Promise(resolve => setTimeout(resolve, 10000))
 
   log('debug', ['index', 'stopping rqlite server'])
-  await stopServer()
+  await rqlite.stopServer()
+
+  await ip.deleteIp('wg0', '10.50.0.20/24')
 
   log('info', ['index', 'deleting interface'])
-  await deleteInterface('wg0')
+  await ip.deleteInterface('wg0')
 })().catch(error => {
   log('error', ['index', 'Failed in execution!', 'Error', error])
   console.error(error)
--- backend/src/lib/iproute2.ts
+++ backend/src/lib/iproute2.ts
@@ -6,6 +6,7 @@ const exec = promisify(execCallback)
 
 const validWgInterface = (name: string): boolean => /^wg\d+$/.test(name)
 const validInterface = (name: string): boolean => /^[a-z0-9]+$/i.test(name)
+const validIPv4 = (ip: string): boolean => /^(?:\d{1,3}\.){3}\d{1,3}\/\d{2}$/m.test(ip)
 
 export async function createInterface (name: string): Promise<void> {
   if (!validWgInterface(name)) {
@@ -19,7 +20,7 @@ export async function createInterface (name: string): Promise<void> {
     return
   } catch (error) {
     if (error.stderr.includes('File exists') as boolean) {
-      log('debug', ['iproute2', 'createInterface', 'interface already exists'])
+      log('warn', ['iproute2', 'createInterface', 'interface already exists'])
       return
     }
     log('error', ['iproute2', 'createInterface', 'failed to create interface', name, 'error', error])
@@ -39,7 +40,7 @@ export async function deleteInterface (name: string): Promise<void> {
     return
   } catch (error) {
     if (error.stderr.includes('Cannot find device') as boolean) {
-      log('debug', ['iproute2', 'deleteInterface', 'interface is already deleted'])
+      log('warn', ['iproute2', 'deleteInterface', 'interface is already deleted'])
       return
     }
     log('error', ['iproute2', 'deleteInterface', 'failed to delete interface', name, 'error', error])
@@ -62,7 +63,7 @@ export async function getInterfaces (): Promise<ShowIpAddress[]> {
 
 export async function getInterface (name: string): Promise<ShowIpAddress | null> {
   if (!validInterface(name)) {
-    log('error', ['iproute2', 'getInterface', 'wrong format on interface name', 'got', name, 'expected', 'wg0..wgn'])
+    log('error', ['iproute2', 'getInterface', 'wrong format on interface name', 'got', name])
     throw Error('Wrong format on interface name')
   }
   const ipInterfaces = await getInterfaces()
@@ -72,6 +73,52 @@ export async function getInterface (name: string): Promise<ShowIpAddress | null>
     log('warn', ['iproute2', 'getInterface', 'got more than one interface', 'returning first interface'])
   }
   return ipInterface[0]
+}
+
+export async function addIp (name: string, ip: string): Promise<void> {
+  if (!validInterface(name)) {
+    log('error', ['iproute2', 'addIp', 'wrong format on interface name', 'got', name])
+    throw Error('Wrong format on interface name')
+  }
+  if (!validIPv4(ip)) {
+    log('error', ['iproute2', 'addIp', 'wrong format on IP address', 'got', ip, 'expected', 'xxx.xxx.xxx.xxx/xx'])
+    throw Error('Wrong format on interface IP')
+  }
+  try {
+    await exec(`ip address add dev ${name} ${ip}`)
+    log('info', ['iproute2', 'addIp', 'successfully added an IP to', name, 'IP', ip])
+    return
+  } catch (error) {
+    if (error.stderr.includes('File exists') as boolean) {
+      log('warn', ['iproute2', 'addIp', 'ip exists already on interface', name, 'IP', ip])
+      return
+    }
+    log('error', ['iproute2', 'addIp', 'failed to add the IP to the interface', name, 'IP', ip, 'error', error])
+    throw error
+  }
+}
+
+export async function deleteIp (name: string, ip: string): Promise<void> {
+  if (!validInterface(name)) {
+    log('error', ['iproute2', 'deleteIp', 'wrong format on interface name', 'got', name])
+    throw Error('Wrong format on interface name')
+  }
+  if (!validIPv4(ip)) {
+    log('error', ['iproute2', 'deleteIp', 'wrong format on IP address', 'got', ip, 'expected', 'xxx.xxx.xxx.xxx/xx'])
+    throw Error('Wrong format on interface IP')
+  }
+  try {
+    await exec(`ip address delete dev ${name} ${ip}`)
+    log('info', ['iproute2', 'deleteIp', 'successfully deleted an IP from', name, 'IP', ip])
+    return
+  } catch (error) {
+    if (error.stderr.includes('Cannot assign requested address') as boolean) {
+      log('warn', ['iproute2', 'deleteIp', 'ip does not exist on interface', name, 'IP', ip])
+      return
+    }
+    log('error', ['iproute2', 'deleteIp', 'failed to delete the IP from the interface', name, 'IP', ip, 'error', error])
+    throw error
+  }
 }
 
 export interface AddrInfo {
