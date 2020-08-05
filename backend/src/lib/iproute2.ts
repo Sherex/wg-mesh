4cd09a5833df5093ecf6c5553e772787b6d9aea5
Sherex <ingar@i-h.no>
2020-08-05 21:29:28 +0200

    Updated logging

--- backend/src/lib/iproute2.ts
+++ backend/src/lib/iproute2.ts
@@ -8,40 +8,40 @@ const validInterface = (name: string): boolean => /^wg\d+$/.test(name)
 
 export async function createInterface (name: string): Promise<void> {
   if (!validInterface(name)) {
-    log('error', ['wireguard', 'createInterface', 'wrong format on interface name', 'got', name, 'expected', 'wg0..wgn'])
+    log('error', ['iproute2', 'createInterface', 'wrong format on interface name', 'got', name, 'expected', 'wg0..wgn'])
     throw Error('Wrong format on interface name')
   }
-  log('debug', ['wireguard', 'createInterface', 'creating interface', name])
+  log('debug', ['iproute2', 'createInterface', 'creating interface', name])
   try {
     await execP(`ip link add dev ${name} type wireguard`)
-    log('info', ['wireguard', 'createInterface', 'successfully created interface', name])
+    log('info', ['iproute2', 'createInterface', 'successfully created interface', name])
     return
   } catch (error) {
     if (error.stderr.includes('File exists') as boolean) {
-      log('debug', ['wireguard', 'createInterface', 'interface already exists'])
+      log('debug', ['iproute2', 'createInterface', 'interface already exists'])
       return
     }
-    log('error', ['wireguard', 'createInterface', 'failed to create interface', name, 'error', error])
+    log('error', ['iproute2', 'createInterface', 'failed to create interface', name, 'error', error])
     throw error
   }
 }
 
 export async function deleteInterface (name: string): Promise<void> {
   if (!validInterface(name)) {
-    log('error', ['wireguard', 'deleteInterface', 'wrong format on interface name', 'got', name, 'expected', 'wg0..wgn'])
+    log('error', ['iproute2', 'deleteInterface', 'wrong format on interface name', 'got', name, 'expected', 'wg0..wgn'])
     throw Error('Wrong format on interface name')
   }
-  log('debug', ['wireguard', 'deleteInterface', 'deleting interface', name])
+  log('debug', ['iproute2', 'deleteInterface', 'deleting interface', name])
   try {
     await execP(`ip link delete ${name}`)
-    log('info', ['wireguard', 'deleteInterface', 'successfully deleted interface', name])
+    log('info', ['iproute2', 'deleteInterface', 'successfully deleted interface', name])
     return
   } catch (error) {
     if (error.stderr.includes('Cannot find device') as boolean) {
-      log('debug', ['wireguard', 'createInterface', 'interface is already deleted'])
+      log('debug', ['iproute2', 'createInterface', 'interface is already deleted'])
       return
     }
-    log('error', ['wireguard', 'deleteInterface', 'failed to delete interface', name, 'error', error])
+    log('error', ['iproute2', 'deleteInterface', 'failed to delete interface', name, 'error', error])
     throw error
   }
 }
