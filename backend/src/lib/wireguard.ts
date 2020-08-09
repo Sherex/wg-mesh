290a0433d58e27f5bc6150143403ea0b393712d0
Sherex <ingar@i-h.no>
2020-08-09 22:03:42 +0200

    Created setConfig()

--- backend/src/lib/wireguard.ts
+++ backend/src/lib/wireguard.ts
@@ -1,6 +1,8 @@
 import { exec as execCallback } from 'child_process'
 import { promisify } from 'util'
 import { log } from './logger'
+import { validWgInterface } from './iproute2'
+import * as tempFile from './temp-file'
 
 const exec = promisify(execCallback)
 
@@ -64,7 +66,7 @@ function parseString (valueString: string, type: 'host' | 'peer' = 'host'): Host
 
   const parsedInfo: {[key: string]: string | number | null} = {}
   const values = valueString.split('\t')
-
+  // TODO: parse allowedIps as an array of ips
   keyString.split('\t').forEach((key, i) => {
     let value: string | number | null = values[i] === '(none)' || values[i] === '' ? null : values[i]
     if (['latestHandshake', 'transferRx', 'transferTx', 'persistentKeepalive'].includes(key)) value = Number(value)
@@ -95,3 +97,69 @@ export function isPeerInfo (data: any): data is PeerInfo {
   }
   return true
 }
+
+export interface ConfigOptions {
+  listenPort?: number
+  fwmark?: string
+  privateKey?: string
+}
+
+export async function setConfig (wgInterface: string, options: ConfigOptions): Promise<void> {
+  if (!validWgInterface(wgInterface)) {
+    log('error', ['wireguard', 'setConfig', 'wrong format on interface name', 'got', wgInterface, 'expected', 'wg0..wgn'])
+    throw Error('Wrong format on interface name')
+  }
+  let configString: string = wgInterface
+  let file
+
+  for (let [key, value] of Object.entries(options)) {
+    let stringVal = String(value)
+    key = key === 'listenPort' ? 'listen-port' : key
+    key = key === 'privateKey' ? 'private-key' : key
+
+    if (key === 'private-key') {
+      file = await tempFile.save(stringVal)
+      stringVal = file.path
+    }
+
+    configString += ` ${key} ${stringVal}`
+  }
+
+  try {
+    await exec(`wg set ${configString}`)
+    log('debug', ['wireguard', 'setConfig', 'successfully set config for', wgInterface])
+  } catch (error) {
+    log('error', ['wireguard', 'setConfig', 'failed to set config for', wgInterface, 'error', error.message])
+    throw error
+  } finally {
+    if (typeof file !== 'undefined') await file.delete()
+  }
+}
+
+setConfig('wg0', {
+  fwmark: 'off',
+  listenPort: 20227,
+  privateKey: 'YA/ON7SSmVsYk+PKXHX8ayryerilzCUJDiOEMATmh2k='
+}).then(console.log).catch(console.error)
+export interface AddPeersOptions {
+  base64PublicKey: string
+  presharedKey?: string
+}
+
+export interface PeerConfigOptions {
+  listenPort?: string
+  fwmark?: string
+  privateKey?: string
+}
+
+export async function addPeer (options: AddPeersOptions): Promise<void> {
+  // TODO: Create addPeer()
+}
+
+export async function deletePeer (base64PublicKey: string): Promise<void> {
+  // TODO: Create deletePeer()
+}
+
+// Options:
+// set <interface> [listen-port <port>] [fwmark <fwmark>] [private-key <file-path>] [peer <base64-public-key> [remove] [preshared-key <file-path>] [endpoint <ip>:<port>]
+// [persistent-keepalive  <interval seconds>] [allowed-ips <ip1>/<cidr1>[,<ip2>/<cidr2>]...] ]...
